b739453de3011f72c2feadddc99948e2bdece82c
Deleon <Adrian.Deleon@externals.adidas.com>
2020-02-19 16:53:15 -0800

    testing commit from vs

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React or dont im not your boss
+          Learn React
         </a>
       </header>
     </div>
