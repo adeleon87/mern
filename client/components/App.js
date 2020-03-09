ff58d984819f75f4b703a9ac63a041abc1721f60
Deleon <Adrian.Deleon@externals.adidas.com>
2020-03-09 14:56:54 -0700

    making client/server division explicit, +webpack

--- client/components/App.js
+++ client/components/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../css/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>client/components/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -16,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React or not, you dont have to
         </a>
       </header>
     </div>
